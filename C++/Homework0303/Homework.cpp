5ed94939ebbbfb2d97c98c1a9cb493d455236797
Albert Gizetdinov <albert160616@gmail.com>
2021-03-03 08:56:28 +0300

    Homework0303

--- C++/Homework0303/Homework.cpp
+++ C++/Homework0303/Homework.cpp
@@ -4,5 +4,25 @@ using namespace std;
 
 int main()
 {
+    cout << "First task" << endl;
+    Stack * stack = new Stack;
+    stack->Push(2);
+    stack->Push(4);
+    stack->Push(6);
+    stack->printAll();
 
+    cout << "Second task" << endl;
+    LinkedList * list = new LinkedList;
+    list->add(1);
+    list->add(3);
+    list->add(5);
+    list->add(7);
+    cout << list->size() << endl;
+    cout << list->get(2) << " " << list->get(4) << endl;
+
+    cout << "Third task" << endl;
+    list->printAll();
+
+
+    return 0;
 }
--- C++/Homework0303/Homework.h
+++ C++/Homework0303/Homework.h
@@ -1,3 +1,93 @@
 #pragma once
+#include "iostream"
 using namespace std;
 
+struct Node
+{
+    int item;
+    Node * next;
+};
+
+struct Stack
+{
+    Node * pHead;
+
+    void Push(int item)
+    {
+        Node * node = new Node;
+        node->item = item;
+        node->next = pHead;
+        pHead = node;
+    }
+
+    int Pop()
+    {
+        if (!pHead)
+            throw 1;
+
+        Node * temp = pHead;
+        int x = temp->item;
+        pHead = temp->next;
+        delete temp;
+        return x;
+    }
+
+    int Peek()
+    {
+        if (!pHead)
+            throw 1;
+
+        return pHead->item;
+    }
+
+    void printAll() {
+        Node * temp = pHead;
+        while (temp != nullptr) {
+            cout << temp->item << endl;
+            temp = temp->next;
+        }
+    }
+};
+
+struct LinkedList
+{
+    Node * head = nullptr;
+    Node * last;
+    int count = 0;
+
+    void add(int num) {
+        Node * node = new Node;
+        node->item = num;
+        if (head == nullptr) {
+            node->next = nullptr;
+            head = node;
+            last = node;
+        } else {
+            last->next = node;
+            last = node;
+        }
+        count++;
+    }
+
+    int size() {
+        return count;
+    }
+
+    int get(int i) {
+        Node * node = head;
+        i--;
+        while (i > 0) {
+            i--;
+            node = node->next;
+        }
+        return node->item;
+    }
+
+    void printAll() {
+        Node * temp = head;
+        while (temp != nullptr) {
+            cout << temp->item << " ";
+            temp = temp->next;
+        }
+    }
+};
